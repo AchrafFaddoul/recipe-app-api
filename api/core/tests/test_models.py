7e441351f6c673b854f80ae6b4c414b5dbf97a30
1337 <faddoulachraf39@gmail.com>
2021-03-22 23:22:09 +0100

    create ther models unittest

---
+++ api/core/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_user_with_email_succesful(self):
+        "Test creating a new user with an email is successful"
+        email = 'test@qbeast.com'
+        password = 'Test@pass123++'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
