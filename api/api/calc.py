21ee82c6e37c303767fec6af4800dc98ac46ff0e
1337 <faddoulachraf39@gmail.com>
2021-03-22 22:05:42 +0100

    fix lintting errors

--- api/api/calc.py
+++ api/api/calc.py
@@ -1,3 +1,8 @@
 def add(x, y):
-    # add two numbers together
+    # add two numbers together and return sum
     return x + y
+
+
+def subtract(x, y):
+    # subtract two numbers and return value
+    return x - y
