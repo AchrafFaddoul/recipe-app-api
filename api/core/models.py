4bb5767dc1e47792d864592e01990147e659862d
1337 <faddoulachraf39@gmail.com>
2021-03-23 17:44:03 +0100

    normalize email before paasing it as a model arg

--- api/core/models.py
+++ api/core/models.py
@@ -6,7 +6,7 @@ class UserManager(BaseUserManager):
     
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
