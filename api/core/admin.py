6c4171f1a8210dae6d602080fa5e7d6aafe66706
1337 <faddoulachraf39@gmail.com>
2021-03-24 18:28:56 +0100

    modify django admin to support change user model

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from django.utils.translation import gettext as _
+
 from core import models
 
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
 
 
 admin.site.register(models.User, UserAdmin)
