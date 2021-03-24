a482735ebfcd93d9081aebb386c9bc8f757dd316
1337 <faddoulachraf39@gmail.com>
2021-03-24 17:19:14 +0100

    check if email value True

--- api/core/models.py
+++ api/core/models.py
@@ -6,6 +6,8 @@ class UserManager(BaseUserManager):
     
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
+        if not email:
+            raise ValueError('users most have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
