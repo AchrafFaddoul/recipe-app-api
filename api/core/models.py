a150608cb8184f6208ae67846829ef60193e5646
1337 <faddoulachraf39@gmail.com>
2021-03-24 17:30:52 +0100

    create superuser and save it

--- api/core/models.py
+++ api/core/models.py
@@ -13,7 +13,15 @@ class UserManager(BaseUserManager):
         user.save(using=self.db)
 
         return user
+    
+    def create_superuser(self, email, password):
+        """create and save new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
 
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that support using email instead of username"""
