3c092ac18681f8c8fd4105e73c17a3a825e81f2f
1337 <faddoulachraf39@gmail.com>
2021-03-25 14:41:08 +0100

    add feature creating user from django admin

--- api/core/admin.py
+++ api/core/admin.py
@@ -18,6 +18,12 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)})
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
