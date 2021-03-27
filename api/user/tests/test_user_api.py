7181aeca8e9bc7fb9eaeb961b2e999ef7fccc3c1
1337 <faddoulachraf39@gmail.com>
2021-03-27 23:54:20 +0100

    add name property to payload and fix code style error

--- api/user/tests/test_user_api.py
+++ api/user/tests/test_user_api.py
@@ -39,7 +39,11 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating user that already exists fails"""
-        payload = {'email': 'test@qbeast.com', 'password': 'testpass@1'}
+        payload = {
+            'email': 'test@qbeast.com',
+            'password': 'testpass@1',
+            'name': 'Test'
+        }
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
@@ -47,7 +51,11 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
-        payload = {'email': 'test@qbeast.com', 'password': 'tst'}
+        payload = {
+            'email': 'test@qbeast.com',
+            'password': 'tst',
+            'name': 'Test'
+        }
 
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
