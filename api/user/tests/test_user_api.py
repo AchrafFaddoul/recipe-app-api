c4d59303723a48df8f611997b6b848b93d05cee7
1337 <faddoulachraf39@gmail.com>
2021-03-27 23:38:05 +0100

    fix code style error

--- api/user/tests/test_user_api.py
+++ api/user/tests/test_user_api.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.test.client import FakePayload
 from django.urls import reverse
 
 from rest_framework.test import APIClient
@@ -8,15 +7,17 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 
+
 def create_user(**param):
     return get_user_model().objects.create_user(**param)
+
 
 class PublicUserApiTests(TestCase):
     """Test the user API (public)"""
 
     def setUp(self):
         self.client = APIClient()
-    
+
     def test_create_valid_user_success(self):
         """Test creating user with valid payload is successful"""
         payload = {
@@ -43,11 +44,11 @@ class PublicUserApiTests(TestCase):
 
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
         payload = {'email': 'test@qbeast.com', 'password': 'tst'}
-        
+
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
