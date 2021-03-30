21f98599522f1ad3c82b26354c44ed17dbec87d1
1337 <faddoulachraf39@gmail.com>
2021-03-30 22:01:55 +0100

    add list tag endpoint feature

--- api/api/urls.py
+++ api/api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
