bb5c206b8531cbcc282543b11ad504d5f750f4e2
afaddoul <faddoulachraf39@gmail.com>
2021-04-12 00:47:33 +0000

    create ingredient serializer

--- api/recipe/serializers.py
+++ api/recipe/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -7,5 +7,13 @@ class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredient objects"""
+
+    class Meta:
+        model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
