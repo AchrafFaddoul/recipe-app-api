8bfba2c8f04ac2f41a05f1e1bad5b41aebaa09fd
afaddoul <faddoulachraf39@gmail.com>
2021-04-14 00:42:03 +0000

    implement feature for listing recipes

--- api/recipe/urls.py
+++ api/recipe/urls.py
@@ -7,6 +7,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredient', views.IngredientViewSet)
+router.register('recipe', views.RecipeViewSet)
 
 app_name = 'recipe'
 
