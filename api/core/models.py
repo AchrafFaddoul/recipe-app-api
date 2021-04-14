9671c6bd457ee00f2071addae9ede39ceea6b7b3
afaddoul <faddoulachraf39@gmail.com>
2021-04-14 00:41:20 +0000

    fix typo tags column name

--- api/core/models.py
+++ api/core/models.py
@@ -72,7 +72,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
-    tag = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
