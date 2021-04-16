b84c710b267938d9191c14bed2b0f406d8544d00
1337 <faddoulachraf39@gmail.com>
2021-04-16 02:23:28 +0000

    fix typo recipes endpoint name

--- api/recipe/urls.py
+++ api/recipe/urls.py
@@ -7,7 +7,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredient', views.IngredientViewSet)
-router.register('recipe', views.RecipeViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
