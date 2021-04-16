e684fff4b91b446709a78d3cae18cdb10801cdc7
1337 <faddoulachraf39@gmail.com>
2021-04-16 01:11:24 +0000

    dd pillow requirement and setup static and media file envirement

--- api/api/settings.py
+++ api/api/settings.py
@@ -126,5 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
 
 AUTH_USER_MODEL = 'core.User'
